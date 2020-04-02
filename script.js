c9d6e7a001932c4db7db01379c015bddf4ed015c
umbridge <44002678+umbridge@users.noreply.github.com>
2020-04-02 19:08:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function myclick(v) {
+    myform.display.value+=v;
+}
+function equalto() {
+   myform.display.value=eval(myform.display.value); 
+}
+function ac( ) {
+    myform.display.value="0";
+}
